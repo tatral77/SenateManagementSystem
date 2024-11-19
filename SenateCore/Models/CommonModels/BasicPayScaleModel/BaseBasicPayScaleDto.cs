056e8c603e1cd636a198d7177fb6e079e3a8c89f
husnain <husnain@email.com>
2024-11-19 15:28:14 +0500

    Changes In Models

---
+++ SenateCore/Models/CommonModels/BasicPayScaleModel/BaseBasicPayScaleDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SenateCore.Models.CommonModels.BasicPayScaleModel
+{
+    public class BaseBasicPayScaleDto
+    {
+        public int Description { get; set; }
+        public bool IsActive { get; set; }
+       
+    }
+}
---
+++ SenateCore/Models/CommonModels/BillStatusModel/BaseBillStatusDto.cs
@@ -0,0 +1,17 @@
+﻿using SenateData.DataModels.Legislation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SenateCore.Models.CommonModels.BillStatusModel
+{
+    public class BaseBillStatusDto
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public bool IsActive { get; set; }
+        public IList<BillDto> Bills { get; set; }
+    }
+}
