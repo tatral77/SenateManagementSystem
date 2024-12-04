002105cdd98c4ea9575d3e26c6d2f041d3928b0c
husnain <husnain@email.com>
2024-12-04 15:59:12 +0500

    Changes

---
+++ SenateCore/Repository/Common/ElectionTypeRepo.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using SenateCore.Contracts.Common;
+using SenateData.DataModels;
+using SenateData.DataModels.Common;
+namespace SenateCore.Repository.Common
+{
+    public class ElectionTypeRepo: GenericRepo<ElectionType>, IElectionTypeRepo
+    {
+        private readonly SenateDBContext _context;
+        private readonly IMapper _mapper;
+
+        public ElectionTypeRepo(SenateDBContext context, IMapper mapper) : base(context, mapper)
+        {
+            this._context = context;
+            this._mapper = mapper;
+        }
+    }
+}
